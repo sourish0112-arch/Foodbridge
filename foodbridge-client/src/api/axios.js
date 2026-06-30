15841088ef8a7267f715c054bdf749ac4810cf72
sourish0112-arch <sourish011@gmail.com>
2026-07-01 01:08:38 +0530

    Fixed socket export and API connection

--- foodbridge-client/src/api/axios.js
+++ foodbridge-client/src/api/axios.js
@@ -2,9 +2,11 @@ import axios from "axios";
 import { io } from "socket.io-client";
 
 const API = axios.create({
-    baseURL: "https://foodbridge-43uu.onrender.com"
+  baseURL: "https://foodbridge-43uu.onrender.com",
 });
 
-export const socket = io("https://foodbridge-43uu.onrender.com");
+export const socket = io("https://foodbridge-43uu.onrender.com", {
+  transports: ["websocket"],
+});
 
 export default API;
